423c83e9cbe648368ac18bc745a7ca0125bccebe
aaron061411 <aaronmora228@gmail.com>
2025-05-22 23:17:15 -0600

    Funcion Suma terminada + incorporar gitignore

--- calculator.cpp
+++ calculator.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int suma(int a, int b)
+{
+    return a + b;
+}
+
 int main ()
 
 {
 
-cout << "Hello, clase!" << endl;
+cout << "Hola Bienvenido" << endl;
+
+cout << "La suma de 1 y 2 es: " << suma(1, 2) << endl;
     return 0;
 }
